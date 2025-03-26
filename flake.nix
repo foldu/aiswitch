0211f7d1bda8fbf6ca9ce964a51ec04bd00bb6b7
foldu <foldu@protonmail.com>
2025-03-26 22:28:12 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Building static binaries with musl";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p:
+          p.rust-bin.stable.latest.default.override {
+            targets = [ "x86_64-unknown-linux-musl" ];
+          }
+        );
+        unfilteredRoot = ./.;
+        lib = pkgs.lib;
+
+        aiswitch = craneLib.buildPackage {
+          src = lib.fileset.toSource {
+            root = unfilteredRoot;
+            fileset = lib.fileset.unions [
+              # Default files from crane (Rust and cargo files)
+              (craneLib.fileset.commonCargoSources unfilteredRoot)
+              # Also keep any html files
+              (lib.fileset.fileFilter (file: file.hasExt "html") unfilteredRoot)
+            ];
+          };
+
+          strictDeps = true;
+
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+        };
+      in
+      {
+        checks = {
+          inherit aiswitch;
+        };
+
+        packages.default = aiswitch;
+      }
+    );
+}
